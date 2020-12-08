f3588a44657f2803788f70e843d3a88b11278d9f
Zaza Saki <isac.cruzn@gmail.com>
2020-12-08 23:32:01 +0000

    Server to build the birdge between javaScript and Python

---
+++ API.py
@@ -0,0 +1,29 @@
+  
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import Setup as sss
+
+class Serv(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        sss.ActionSetup(self.path)
+        if self.path!='/log':
+            print(self.path)
+            pass
+        
+        if self.path == '/':
+            self.path = '/index.html'
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            file_to_open = "File not found"
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
+
+def launch():
+    httpd = HTTPServer(('localhost', 8080), Serv)
+    httpd.serve_forever()
+    pass
+
+launch()
---
+++ Setup.py
@@ -0,0 +1,44 @@
+from numpy.lib.type_check import common_type
+import PredictionGraphGenerator as gmath
+
+def extractRequest(path):
+    request = path.split("/",1)[1].split("?",1)
+    comand = request[0]
+    args = request[1].split(";")
+
+    print("comand : " + comand)
+    print("args : ", args[0])
+
+    return(comand, args)
+
+
+def extractArray(string):
+    Array =[]
+    for target_list in string.split(","):
+        Array.append(float(target_list))
+    
+    return Array
+
+def saveFile(list):
+    f = open("Days.json", "w")
+    f.write("{\"List\":[" + list + "]}")
+    f.close()
+    pass
+
+
+def ActionSetup(path):
+    comand, args = extractRequest(path);
+
+    if comand == "logGraph":
+        gmath.LogRegressionGraph(extractArray(args[0]))
+
+    if comand == "drawGraph":
+        gmath.DrawGraph(extractArray(args[0]));
+        pass
+    
+    if comand == "save":
+        saveFile(args[0])
+        pass
+
+    pass
+
