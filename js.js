6b80f75d5733dfec0848dbbc60e06b3db79e94b2
feckboiinc <48067114+feckboiinc@users.noreply.github.com>
2020-12-09 21:42:42 +0000

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+//Abre o Menu
+  function openNav() {
+  document.getElementById("myNav").style.width ="100%";
+}
+//Fecha o Menu
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
