42dddcfe3318aed8f58fd5585abd7aae0cc8252d
feckboiinc <48067114+feckboiinc@users.noreply.github.com>
2020-12-10 23:04:56 +0000

    Add files via upload

--- js.js
+++ js.js
@@ -7,18 +7,10 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
-function addItem(){
-	 var ul = document.getElementById("dynamic-list");
-	 var candidate = document.getElementById("candidate");
-	 var li = document.createElement("li");
-	 li.setAttribute('id',candidate.value);
-	 li.appendChild(document.createTextNode(candidate.value));
-	 ul.appendChild(li);
-	}
+function hidden(){
 	
-	function removeItem(){
-		var ul = document.getElementById("dynamic-list");
-		var candidate = document.getElementById("candidate");
-		var item = document.getElementById(candidate.value);
-		ul.removeChild(item);
-	}
+	if(Med.nDays()==0){
+	  button.hide=true;
+	}else
+	  button.hide=false;
+}
